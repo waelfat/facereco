9a8d798b5fb568d7e1910c9261f3ef0bf20f070e
waelfat <waelfathy2007@gmail.com>
2024-12-04 21:24:53 +0200

    addoctet

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, Request, UploadFile, File, HTTPException
 from app.utils.face_recognition import encode_face, compare_faces
 from app.utils.file_storage import load_metadata, load_encoding
-
+from PIL import Image
+from io import BytesIO
 router = APIRouter()
+def valid_image(bytes: bytes) -> bool:
+    try:
+        imget=Image.open(BytesIO(bytes))
+        imget.verify()
+        return True
+    except:
+        return False
 
 @router.post("/login")
 async def login(image: UploadFile = File(...)):
@@ -20,3 +28,19 @@ async def login(image: UploadFile = File(...)):
         raise HTTPException(status_code=401, detail="Face not recognized")
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+@router.post("/login-octet-stream")
+async def login_octet_stream(request:Request):
+    data = await request.body()
+    if not valid_image(data):
+        raise HTTPException(status_code=400, detail="Invalid image format. Only PNG, JPG, and JPEG are allowed.")
+    try:
+        face_encoding = encode_face(data)
+        metadata = load_metadata()
+        for employee_id, details in metadata.items():
+            known_face_encoding = load_encoding(details['encoding_filename'])
+            if compare_faces(known_face_encoding, face_encoding):
+                return {"employee_id": employee_id, "name": details['name']}
+        raise HTTPException(status_code=401, detail="Face not recognized")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+   
--- app/api/endpoints/register.py
+++ app/api/endpoints/register.py
@@ -1,8 +1,25 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException, Form
+from fastapi import APIRouter, Query, Request, UploadFile, File, HTTPException, Form
 from app.utils.face_recognition import encode_face
 from app.utils.file_storage import load_metadata, save_encoding, save_metadata
-
+from PIL import Image
+from io import BytesIO
 router = APIRouter()
+def valid_image(bytes: bytes) -> bool:
+    try:
+        imget=Image.open(BytesIO(bytes))
+        imget.verify()
+        return True
+    except:
+        return False
+def get_unique_filename(employee_id: str, extension: str) -> str:
+    return f"{employee_id}.{extension}"
+def save_image(bytes: bytes, filename: str) -> bool:
+    try:
+        imget=Image.open(BytesIO(bytes))
+        imget.save(filename)
+        return True
+    except:
+        return False
 
 @router.post("/register")
 async def register_employee(
@@ -25,4 +42,32 @@ async def register_employee(
         save_metadata(employee_id, name, encoding_filename)
         return {"employee_id": employee_id, "name": name}
     except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+
+@router.post("/register-octet-stream")
+async def register_employee_octet_stream(
+     request:Request,
+    employee_id:str =    Query(...),
+    name:str = Query(...)
+):
+     if not employee_id or not name:
+        raise HTTPException(status_code=400, detail="Missing required fields")
+     
+     data = await request.body()
+     if not data:
+        raise HTTPException(status_code=400, detail="Missing image")
+     if not valid_image(data):
+        raise HTTPException(status_code=400, detail="Invalid image format. Only PNG, JPG, and JPEG are allowed.")
+     
+     metadata = load_metadata()
+
+     if employee_id in metadata:
+        raise HTTPException(status_code=400, detail="Employee ID already exists")
+     try:
+        face_encoding = encode_face(data)
+        encoding_filename = save_encoding(employee_id, face_encoding)
+        save_metadata(employee_id, name, encoding_filename)
+        return {"employee_id": employee_id, "name": name}
+     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
